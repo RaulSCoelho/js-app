395335d10038f231e59309dc706c7695872deb6b
Raul Semicek Coelho <raul.coelho@nkey.com.br>
2025-04-20 14:46:25 -0300

    js-app

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,9 +1,34 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@raulscoelho/eslint-config/next'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'import-helpers'],
   rules: {
     'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        trailingComma: 'none',
+        endOfLine: 'auto',
+        printWidth: 120,
+        tabWidth: 2
+      }
+    ],
+    'import/no-duplicates': ['error', { 'prefer-inline': true }],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['/^react/', 'module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
   }
 }
--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,11 +1,51 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@raulscoelho/eslint-config/node'],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'import-helpers'],
+  env: {
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    camelcase: 'off'
+    camelcase: 'off',
+    'no-useless-constructor': 'off',
+    'no-case-declarations': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        trailingComma: 'none',
+        endOfLine: 'auto',
+        printWidth: 120,
+        tabWidth: 2
+      }
+    ],
+    'import/no-duplicates': ['error', { 'prefer-inline': true }],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts']
+    }
   }
 }
